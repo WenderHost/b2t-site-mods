a0395a4e5ee831188b00494a5239df5edfb0db75
Michael Wender <michael@michaelwender.com>
2023-03-28 10:30:59 -0400

    First commit

---
+++ b2t-site-mods.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     B2T Site Mods
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     Various extensions to the B2T Training site.
+ * Author:          Michael Wender
+ * Author URI:      https://wenmarkdigital.com
+ * Text Domain:     b2t-site-mods.php
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         B2t_Site_Mods.php
+ */
+
+// Your code starts here.
